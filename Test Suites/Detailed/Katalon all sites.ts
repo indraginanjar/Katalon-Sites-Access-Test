673c5d1455dcbbfee36f83d6400463550116886d
Indra Ginanjar <indraginanjar@gmail.com>
2023-07-02 15:46:46 +0700

    Katalon Sites Access Test: Test Suites: 'Behavioral Katalon all sites' become 'Katalon all sites' and non cucumber runner test suites, moved to Detailed/ folder

--- Test Suites/Detailed/Katalon all sites.ts
+++ Test Suites/Detailed/Katalon all sites.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Behavioral Katalon all sites</name>
+   <name>Katalon all sites</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,33 +10,40 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>4d2c0a90-32ba-4c49-b4eb-b7453ea4d042</testSuiteGuid>
+   <testSuiteGuid>1f499831-53ae-4342-8466-3a3a1cdb2374</testSuiteGuid>
    <testCaseLink>
-      <guid>d250f63b-7a80-4aea-9bee-04bb49047573</guid>
+      <guid>1198b45c-75f9-41c4-8401-59e68265d52f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Runners/Katalon website</testCaseId>
+      <testCaseId>Test Cases/Tests/Open web browser successfuly</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>1d0f50d9-56d6-44d6-b9b2-89543ab5082b</guid>
+      <guid>8087cf71-ba1d-43c8-986a-6149809f3bc8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Runners/Katalon admin site</testCaseId>
+      <testCaseId>Test Cases/Tests/Katalon website is opened succesfully</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>85301d9d-09ba-44e1-984c-a0e985fddfda</guid>
+      <guid>1f2a217b-e46e-4443-a744-5f97a5752c64</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Runners/Katalon testops site</testCaseId>
+      <testCaseId>Test Cases/Tests/Katalon admin site is opened succesfully</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>34c3456b-ba1a-47d0-aba3-0cf7d79fb8d7</guid>
+      <guid>19e0c413-9000-4516-81eb-13b799d5b67b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Runners/Katalon store site</testCaseId>
+      <testCaseId>Test Cases/Tests/Katalon testops site is opened succesfully</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2426ae3-5faa-4b06-acef-ffaf870f857b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Tests/Katalon store site is opened succesfully</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/Katalon all sites.ts
+++ Test Suites/Katalon all sites.ts
@@ -10,40 +10,33 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>1f499831-53ae-4342-8466-3a3a1cdb2374</testSuiteGuid>
+   <testSuiteGuid>4d2c0a90-32ba-4c49-b4eb-b7453ea4d042</testSuiteGuid>
    <testCaseLink>
-      <guid>1198b45c-75f9-41c4-8401-59e68265d52f</guid>
+      <guid>d250f63b-7a80-4aea-9bee-04bb49047573</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Tests/Open web browser successfuly</testCaseId>
+      <testCaseId>Test Cases/Runners/Katalon website</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>8087cf71-ba1d-43c8-986a-6149809f3bc8</guid>
+      <guid>1d0f50d9-56d6-44d6-b9b2-89543ab5082b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Tests/Katalon website is opened succesfully</testCaseId>
+      <testCaseId>Test Cases/Runners/Katalon admin site</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>1f2a217b-e46e-4443-a744-5f97a5752c64</guid>
+      <guid>85301d9d-09ba-44e1-984c-a0e985fddfda</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Tests/Katalon admin site is opened succesfully</testCaseId>
+      <testCaseId>Test Cases/Runners/Katalon testops site</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>19e0c413-9000-4516-81eb-13b799d5b67b</guid>
+      <guid>34c3456b-ba1a-47d0-aba3-0cf7d79fb8d7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Tests/Katalon testops site is opened succesfully</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d2426ae3-5faa-4b06-acef-ffaf870f857b</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Tests/Katalon store site is opened succesfully</testCaseId>
+      <testCaseId>Test Cases/Runners/Katalon store site</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
